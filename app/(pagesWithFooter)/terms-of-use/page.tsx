94f6c57738853549f9a3711f4d1b3018d9d7e3ee
Horacio Gutierrez <horacio.estevez@gmail.com>
2023-12-18 13:39:24 -0300

    Update social media icons and add links to profile pages

---
+++ app/(pagesWithFooter)/terms-of-use/page.tsx
@@ -0,0 +1,29 @@
+export default function page() {
+  return (
+    <main className='grow'>
+      <div className="container">
+        <section id="terms-of-use">
+          <h2 className="font-bold text-2xl mb-1">Términos y Condiciones de Uso</h2>
+          <p className="text-slate-400 mb-4 font-light">Bienvenidos a <span className="text-slate-200 font-normal">CarayanIA</span>, la aplicación de chatbot con IA que te permite interactuar con una versión virtual de personalidades famosas (el "Servicio"). Estos Términos y Condiciones de Uso regulan su uso de nuestro Servicio proporcionado por <span className="text-slate-200 font-normal">CarayanIA</span>, ubicada en Argentina.</p>
+
+          <h2 className="font-bold text-2xl mb-1">Aceptación de Términos</h2>
+          <p className="text-slate-400 mb-4 font-light">Al acceder o utilizar nuestro Servicio, usted acepta estar sujeto a estos Términos y Condiciones de Uso. Si no está de acuerdo con alguno de los términos establecidos aquí, no debe usar ni acceder a este Servicio.</p>
+
+          <h2 className="font-bold text-2xl mb-1">Descripción del Servicio</h2>
+          <p className="text-slate-400 mb-4 font-light"><span className="text-slate-200 font-normal">CarayanIA</span> le ofrece la posibilidad de formular hasta 5 preguntas gratuitas a nuestro chatbot con IA, que imita la personalidad de figuras públicas. Si desea realizar más preguntas, puede optar por nuestro plan de pago, el cual incluye 10 preguntas adicionales por un precio de $1000 pesos argentinos.</p>
+
+          <h2 className="font-bold text-2xl mb-1">Registro y Cuenta</h2>
+          <p className="text-slate-400 mb-4 font-light">Para acceder a ciertas funcionalidades del Servicio, es posible que deba registrarse y crear una cuenta. Usted es responsable de mantener la confidencialidad de su cuenta y contraseña, así como de todas las actividades realizadas bajo su cuenta.</p>
+
+          <h2 className="font-bold text-2xl mb-1">Uso del Servicio</h2>
+          <p className="text-slate-400 mb-4 font-light">El uso del Servicio está destinado únicamente a fines de entretenimiento. No se permite el uso de nuestro chatbot para actividades ilegales, difamatorias o que infrinjan los derechos de terceros.</p>
+
+          <h2 className="font-bold text-2xl mb-1">Modificaciones del Servicio y Precios</h2>
+          <p className="text-slate-400 mb-4 font-light"><span className="text-slate-200 font-normal">CarayanIA</span> se reserva el derecho de modificar o discontinuar el Servicio (o cualquier parte del mismo) sin previo aviso en cualquier momento. Asimismo, nos reservamos el derecho de modificar los precios de los planes que ofrecemos.</p>
+
+          <p className="text-slate-400 mb-4 font-light">Lea estos términos completa y cuidadosamente. Al utilizar el Servicio, reafirma su aceptación de los términos vigentes.</p>
+        </section>
+      </div>
+    </main>
+  )
+}
