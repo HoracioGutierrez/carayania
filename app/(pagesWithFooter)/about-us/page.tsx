a2abcb549821318fb5402f7c4e6cc36b40d924da
Horacio Gutierrez <horacio.estevez@gmail.com>
2023-12-21 12:57:21 -0300

    Add "Quienes son?" section to about-us page

--- app/(pagesWithFooter)/about-us/page.tsx
+++ app/(pagesWithFooter)/about-us/page.tsx
@@ -3,7 +3,8 @@ export default function page() {
         <main className='grow'>
             <div className="container">
                 <section id="privacy-policy">
-                    
+                <h2 className="font-bold text-2xl mb-1">Quienes son?</h2>
+                    <p className="text-slate-400 mb-4 font-light"><span className="text-slate-200 font-normal">CarayanIA</span> esta formado por un grupo de programadores farafans muy al pedo. Abajo les dejamos nuestras redes sociales para que puedan hablarnos para ofrecernos algun trabajo , putearnos o mandarnos saludos.</p>
                 </section>
             </div>
         </main>
